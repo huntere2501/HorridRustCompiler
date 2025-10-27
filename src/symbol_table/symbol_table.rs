41591e586aaa12f8efd5b4adf8f4e6e13ed30653
huntere2501 <huntere2501@gmail.com>
2025-10-27 15:55:05 -0500

    Added SymbolTableEntry struct, and funcs to SymbolTable to add, remove, and search.

--- src/symbol_table/symbol_table.rs
+++ src/symbol_table/symbol_table.rs
@@ -1,21 +1,45 @@
-use crate::lexer_scanner::scanner::{Lexer, Token, TokenType};
+use std::collections::HashMap;
+use crate::lexer_scanner::scanner::{TokenType};
 
-pub(crate) struct Symbol_Table<'a> {
-    lexeme: &'a str,
+pub(crate) struct SymbolTableEntry<'a> {
     token_type: &'a TokenType,
     data_type: &'a str,
     value: &'a i64,
     scope: &'a str,
 }
 
-impl <'a> Symbol_Table<'a>{
-    pub fn new(self){
-        0;
+pub(crate) struct SymbolTable<'a> {
+    entries: HashMap<&'a str, SymbolTableEntry<'a>>,
+}
+
+impl <'a> SymbolTableEntry<'a>{
+    pub fn new(token_type: &TokenType, data_type: &str, value: &i64, scope: &str) -> Self{
+        Self {token_type, data_type, value, scope}
     }
-    pub fn add(self){
-        0;
+}
+
+impl <'a> SymbolTable<'a>{
+    pub fn new(entries: HashMap<&'a str, SymbolTableEntry<'a>>){
+        Self{entries};
     }
-    pub fn remove(self){
-        0;
+
+    pub fn insert(&mut self, lexeme: &str, token_type: &TokenType, data_type: &str, value: &i64, scope: &str){
+        self.entries.insert(lexeme, SymbolTableEntry {
+            token_type: &token_type,
+            data_type,
+            value,
+            scope,
+        });
+    }
+    pub fn remove(&mut self, lexeme: &str){
+        self.entries.remove(lexeme);
+    }
+    pub fn search(&mut self, lexeme: &str, token_type: &TokenType, data_type: &str, value: &i64, scope: &str){
+        if self.entries.contains_key(lexeme){
+            // Do Nothing
+        }
+        else{
+            self.insert(lexeme, token_type, data_type, value, scope);
+        }
     }
 }
